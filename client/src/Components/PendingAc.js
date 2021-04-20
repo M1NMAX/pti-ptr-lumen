3858a706c6deac47308da079cd292448846d1188
madalenavi <65404742+madalenavi@users.noreply.github.com>
2021-04-21 00:48:29 +0100

    Pending até bom
    na minha humilde opinião :)

--- client/src/Components/PendingAc.js
+++ client/src/Components/PendingAc.js
@@ -1,32 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { useState, useEffect } from "react";
-import { useHistory } from 'react-router-dom';
-import ReactDOM from "react-dom";
 import {  BrowserRouter as Router,  Switch,  Route} from "react-router-dom";
 import {Navbar, Nav, NavDropdown, Button, Form, Card, Row, Col} from 'react-bootstrap'
 import { AnimationWrapper } from 'react-hover-animation'
 import alojamento from '../img/basicRoom.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import api from '../services/api';
+import DefaultUserPic from "../img/standartUser3.png";
 
 function Accommodation({accom}) {
     
     const x = accom.map((accommodation)=>
-        <Card className="mb-4 mt-4 ml-4 mr-4 center">
+        <Card className="mb-4 mt-4 ml-4 mr-4 center" style={{ width: '30%', height: '10%' }}>
             <AnimationWrapper>
-                <a href="/profileAlojamento">
-                    <Card.Img onclick="href='/profileAlojamento" className="img" src={alojamento}></Card.Img>
+            <a href={"/profileAlojamento/"+accommodation.id}>
+                   <Card.Img onclick="href='/profileAlojamento" className="img" src={alojamento}></Card.Img>
                 </a>
             </AnimationWrapper>
-            <Card.Title className="center">{accommodation.name}</Card.Title>
-
+            
             <Card.Text>
-                <p>{accommodation.description}</p>
-                <p>Preco: {accommodation.price}&euro;</p>
-                <p>Rating: {accommodation.rating}&#42;</p>
+                <p style={{ fontSize: '50%' }}>{accommodation.name}</p>
+                { /*<Button style={{ fontSize: '50%' }}  variant="primary"href={ "/profileAccommodation/"+accommodation.id}>Ver alojamento</Button>*/}
             </Card.Text>
-            <Button variant="primary"href={ "/profileAccommodation/"+accommodation.id}>Ir para o alojamento</Button>
+            
         </Card> 
     )
+    const users = ["Pedro", "checheche"]
     const amount = x.length
     let rows = 0
     if (amount%3 != 0) {
@@ -38,19 +38,26 @@ function Accommodation({accom}) {
     console.log(rows)
     let content = []
     let y = 0
-    for (let i=0; i < rows; i++){
-        content.push(<Row>
-            <Col>
+    for (let i=0; i < 2; i++){
+        content.push(<Row className='border'>
+            <Col></Col>
+
+            <Col className="mb-4 mt-4 ml-4 mr-4 center">
+            <img src={DefaultUserPic} alt="Imagem de perfil" width="30px"></img>
+            <h7 className="d-inline-block">Pedro</h7>{' '}
+            <Button size="sm"><FontAwesomeIcon icon={faEnvelope}/></Button>
+            </Col>
+            <Col className="center">
                 {x[y]}
             </Col>
-            <Col>
-                {x[y+1]}
+            <Col className="mb-4 mt-4 ml-4 mr-4 center">
+            <Button variant="success" size="sm">Aceitar</Button>{' '}
+            <Button variant="danger" size="sm">Rejeitar</Button>{' '}
+            
             </Col>
-            <Col>
-                {x[y+2]}
-            </Col>
+            <Col></Col>
         </Row>)
-        y += 3
+        y += 1
     }
     
     return (
