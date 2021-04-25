b39eaf3ca0590ec7d8d48ca9c42846502238a437
Joãooo <65404823+fc53300Official@users.noreply.github.com>
2021-04-25 23:30:55 +0100

    Basically nothing

--- ApiAccommodation/app/Http/Controllers/AccommodationRequirementsController.php
+++ ApiAccommodation/app/Http/Controllers/AccommodationRequirementsController.php
@@ -3,44 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\AccommodationInfo;
+use App\Models\AccommodationRequirements;
 
-class AccommodationInfoController extends Controller
+class AccommodationRequirementsController extends Controller
 {
-    private $accommodation_info;
+    private $accommodation_requirements;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(AccommodationInfo $accommodation_info)
+    public function __construct(AccommodationRequirements $accommodation_requirements)
     {
-        $this->accommodation_info = $accommodation_info;
+        $this->accommodation_requirements = $accommodation_requirements;
     }
 
 
     public function show($id)
     {
-        return AccommodationInfo::firstOrFail()->where('accommodation_id', $id)->get();
+        return AccommodationRequirements::firstOrFail()->where('accommodation_id', $id)->get();
+        return response()->json(['data' => ['message' => 'Requisitos adicionados com sucesso'], 'status'=>true]);
     }
 
     public function store(Request $request)
     {
-        $this->accommodation_info->create($request->all());
+        $this->accommodation_requirements->create($request->all());
         return response()->json(['data' => ['message' => 'Requisitos adicionados com sucesso'], 'status'=>true]);
     }
 
-    public function update($accommodation_info, Request $request)
+    public function update($accommodation_requirements, Request $request)
     {
-        $accommodation_info = $this->accommodation_info->find($accommodation_info);
-        $accommodation_info->update($request->all());
+        $accommodation_requirements = $this->accommodation_requirements->find($accommodation_requirements);
+        $accommodation_requirements->update($request->all());
         return response()->json(['data' => ['message' => 'Requisitos atualizados com sucesso'], 'status'=>true]);
     }
 
-    public function destroy($accommodation_info)
+    public function destroy($accommodation_requirements)
     {
-        $accommodation_info = $this->accommodation_info->find($accommodation_info);
-        $accommodation_info->delete();
+        $accommodation_requirements = $this->accommodation_requirements->find($accommodation_requirements);
+        $accommodation_requirements->delete();
         return response()->json(['data' => ['message' => 'Requisitos eliminados com sucesso'], 'status'=>true]);
     }
 }
