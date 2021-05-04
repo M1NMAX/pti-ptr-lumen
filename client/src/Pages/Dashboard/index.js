3cb9c30969150525aaf173a871bcba01a42f523a
gmvc2000 <65404606+gmvc2000@users.noreply.github.com>
2021-05-05 00:15:03 +0100

    Developing new Dashboard

--- client/src/Pages/Dashboard/index.js
+++ client/src/Pages/Dashboard/index.js
@@ -54,87 +54,57 @@ function Dashboard() {
       return (
         <div>
           <NavBarHome/>
-          <Card className="text-center">
-              <Card.Header>Dashboard</Card.Header>
-              <Card.Body>
-                  <Card.Title>Bem-vindo, {user.username}</Card.Title>
-                  <Form inline className="searchDashboard">
-                    <Form.Control type="text" placeholder="Onde?(Concelho/Freguesia/Morada)" className="mr-sm-2 search-box" />
-                    <Button variant="primary" className="button"><FontAwesomeIcon icon={faSearch} /></Button>
-                  </Form>
-              </Card.Body>
-          </Card>
-              <Container fluid>
-                <Row className="mb-5 mt-5" xs={6} sm={4}>
-                  <Col xs={6} sm={4}>
-                    <a href="/listChat">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={chatImg}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Veja as suas mensagens!</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
-                  
-                  <Col xs={6} sm={4}>
-                    <a href="/registerAccommodation">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={create}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Adicione um alojamento!</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
-                  
-                  <Col xs={6} sm={4}>
-                    <a href="/pending">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={pending}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Verifique os seus pedidos pendentes!</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
-
-                  <Col xs={6} sm={4}>
-                    <a href="/profile">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={profile}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Aceda ao seu perfil!</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
-                  
-                  <Col xs={6} sm={4}>
-                    <a href="/registerAccommodation">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={edit}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Edite um alojamento!</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
-                  
-                  <Col xs={6} sm={4}>
-                    <a href="/">
-                      <Card className="text-center">
-                        <Card.Img className="imgDashboard" src={home}></Card.Img>
-                        <Card.Body>
-                          <Card.Title>Página inicial</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </a>
-                  </Col>
+          <Container fluid>
+            <Row>
+              <Col sm={12} lg={2} className="sidebar">
+                <Row>
+                  <a href="/listChat">
+                    <img className = "imgDashboard" src={chatImg}></img> Mensagens
+                  </a>
                 </Row>
-              </Container>
-              <Footer></Footer>
+                <Row>
+                  <a href="/registerAccommodation">
+                    <img className = "imgDashboard" src={create}></img> Adicionar Alojamento
+                  </a>
+                </Row>
+                <Row>
+                  <a href="/pending">
+                    <img className = "imgDashboard" src={pending}></img> Pendentes
+                  </a>
+                </Row>
+                <Row>
+                  <a href="/profile">
+                    <img className = "imgDashboard" src={profile}></img> Perfil
+                  </a>
+                </Row>
+                <Row>
+                  <a href="/pending">
+                    <img className = "imgDashboard" src={edit}></img> Editar Alojamento
+                  </a>
+                </Row>
+                <Row>
+                  <a href="/">
+                    <img className = "imgDashboard" src={home}></img> Página Inicial
+                  </a>
+                </Row>
+              </Col>
+              
+              <Col sm={10} lg={10}>
+                <Card className="text-center">
+                    <Card.Body>
+                        <Card.Title>Bem-vindo, {user.username}</Card.Title>
+                        <Form inline className="searchDashboard">
+                          <Form.Control type="text" placeholder="Onde?(Concelho/Freguesia/Morada)" className="mr-sm-2 search-box" />
+                          <Button variant="primary" className="button"><FontAwesomeIcon icon={faSearch} /></Button>
+                        </Form>
+                    </Card.Body>
+                </Card>   
+              </Col>
+            </Row>
+          </Container>
         </div>
+            
+
       )
   } else {
     return (
@@ -226,3 +196,77 @@ function Dashboard() {
 }
 
 export default Dashboard
+
+
+
+/* <Container fluid>
+                  <Row className="mb-5 mt-5" xs={6} sm={4}>
+                    <Col xs={6} sm={4}>
+                      <a href="/listChat">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={chatImg}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Veja as suas mensagens!</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+                    
+                    <Col xs={6} sm={4}>
+                      <a href="/registerAccommodation">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={create}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Adicione um alojamento!</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+                    
+                    <Col xs={6} sm={4}>
+                      <a href="/pending">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={pending}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Verifique os seus pedidos pendentes!</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+
+                    <Col xs={6} sm={4}>
+                      <a href="/profile">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={profile}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Aceda ao seu perfil!</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+                    
+                    <Col xs={6} sm={4}>
+                      <a href="/registerAccommodation">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={edit}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Edite um alojamento!</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+                    
+                    <Col xs={6} sm={4}>
+                      <a href="/">
+                        <Card className="text-center">
+                          <Card.Img className="imgDashboard" src={home}></Card.Img>
+                          <Card.Body>
+                            <Card.Title>Página inicial</Card.Title>
+                          </Card.Body>
+                        </Card>
+                      </a>
+                    </Col>
+                  </Row>
+                </Container>
+                <Footer></Footer>
+                */
