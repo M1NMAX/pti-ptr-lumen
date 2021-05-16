34574bdaf073b2717d24a9861affd7cdf72b7c0f
Joãooo <65404823+fc53300Official@users.noreply.github.com>
2021-05-16 15:50:05 +0100

    Validators and localsearch updated

--- ApiAccommodation/app/Http/Controllers/AccommodationController.php
+++ ApiAccommodation/app/Http/Controllers/AccommodationController.php
@@ -93,6 +93,7 @@ class AccommodationController extends Controller
             'name'=>'required|max:30',
             'description' => 'required',
             'price' => 'required',
+            'location' => 'required',
             'address' => 'required',
             'latitude' => 'required',
             'longitude' => 'required'
@@ -212,8 +213,14 @@ class AccommodationController extends Controller
 
     public function localSearch($search,Request $request)
     {
-        $accommodations = Accommodation::where('location', $search)
-        ->orWhere('location', 'like', '%' . $search . '%')->get();
+        $str = 'null';
+        if($search == $str){
+            $accommodations = Accommodation::get();
+        }else{
+            $accommodations = Accommodation::where('location', $search)
+            ->orWhere('location', 'like', '%' . $search . '%')->get();
+            
+        }
         return $accommodations;
 
     }
