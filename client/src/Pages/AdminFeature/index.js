b740084afce5d0de37fa5d0f3d49210420d3d8f6
madalenavi <65404742+madalenavi@users.noreply.github.com>
2021-05-16 01:24:35 +0100

    admin page feature e mais alguns pormenores

--- client/src/Pages/AdminFeature/index.js
+++ client/src/Pages/AdminFeature/index.js
@@ -3,22 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import api from '../../services/api';
 import React, { useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom';
-import AdminSingleAccommodation from '../../Components/AdminSingleAccom'
+import AdminSingleFeature from '../../Components/AdminSingleFeature'
 import {Container, Card,Row,Col, Form, Button, FormControl} from 'react-bootstrap'
 import Footer from '../../Components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
 
 function AdminPage() {
-    const [Accommodations, setAccommodations] = useState([]);
+    const [Feature, setFeature] = useState([]);
     const [token] = useState(localStorage.getItem('token'));
 
     const history = useHistory();
    
     useEffect(() => {
-        api.get('api/accommodations').then(response => {
+        api.get('api/features').then(response => {
                 console.log(response.data)
-                setAccommodations(response.data);
+                setFeature(response.data);
                 console.log(response.data.length);
             }).catch(err => {
                 alert(err)
@@ -26,8 +26,8 @@ function AdminPage() {
         }, []);
 
 
-    async function remove(accommodationId){
-       await api.delete('api/accommodations/'+accommodationId, {
+    async function remove(featureId){
+       await api.delete('api/feature/'+featureId, {
             headers: {
                 Authentication: `Bearer ${token}`,
             }
@@ -41,23 +41,21 @@ function AdminPage() {
           alert(err)
         })
 
-        setAccommodations(Accommodations);
+        setFeature(Feature);
     }
     
     return(
         <div>
             <NavBarAdmin/>
             <Container>
-                <h3 className="center">Página do Admistrador</h3> 
+                <h3 className="center">Caracteristicas</h3> 
                 <div inline> 
-                    <Button variant="info" className="button mr-sm-2">Filtrar</Button>
-                    <Button variant="info" className="button">Imprimir resultados</Button>
                     <Form inline style={{float:'right', maxWidth:'100%'}}>
                         <Form.Control type="text"  placeholder="Pesquisar" className="mr-sm-2 "/>
                         <Button variant="info" className="button"><FontAwesomeIcon icon={faSearch} /></Button>
                     </Form>
                 </div>
-                {Accommodations.map((accommodation)=>(<AdminSingleAccommodation accom={accommodation} remove={remove} />)) }
+                {Feature.map((feature)=>(<AdminSingleFeature accom={feature} remove={remove} />)) }
             </Container>
             <Footer/>
        </div>
